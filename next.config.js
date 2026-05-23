61ccfa9b25af3ed9e8acb8ec6bfc2017f796cab3
Anurag Mishra <anuragayumishra@gmail.com>
2026-05-24 03:00:18 +0530

    feat: add Sentry SDK integration to Next.js frontend

--- next.config.js
+++ next.config.js
@@ -18,5 +18,37 @@ const nextConfig = {
   },
 }
 
-module.exports = nextConfig
+const { withSentryConfig } = require("@sentry/nextjs");
+
+module.exports = withSentryConfig(
+  nextConfig,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during profiling/building
+    silent: true,
+    org: "quicksnap",
+    project: "quicksnap-frontend",
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Uploads a larger set of source maps for clearer stack traces
+    widenClientBounds: true,
+
+    // Transpiles SDK to be compatible with older browsers
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad blockers
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from public clients
+    hideSourceMaps: true,
+
+    // Automatically tree-shakes Sentry logging in production builds
+    disableLogger: true,
+  }
+);
 
